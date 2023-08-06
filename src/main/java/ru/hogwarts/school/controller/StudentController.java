cdaf588d8a02c4807ee4b6234585f6a390018103
krassot1n <krassotin68@gmail.com>
2023-08-07 03:11:31 +0800

    done lesson 3.4

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -2,12 +2,11 @@ package ru.hogwarts.school.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
 
 import java.util.Collection;
-import java.util.Collections;
+
 
 @RestController
 @RequestMapping("/student")
