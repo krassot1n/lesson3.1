4a62f6657785efa0eb00d78858509a23e659f26e
krassot1n <krassotin68@gmail.com>
2023-09-20 00:55:01 +0800

    reworked lesson 4.6

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -16,25 +16,26 @@ public class StudentService {
     public StudentService(StudentRepository repository) {
         this.repository = repository;
     }
+    private int counter = 0;
 
     Logger logger = LoggerFactory.getLogger(StudentService.class);
 
-    public Student creatStudent(Student student){
+    public Student creatStudent(Student student) {
         logger.debug("Вызван метов создания студента");
         return repository.save(student);
     }
 
-    public Student findStudent(long id){
+    public Student findStudent(long id) {
         logger.debug("Вызван метов поиска студента");
         return repository.findById(id).get();
     }
 
-    public void removeStudent(long id){
+    public void removeStudent(long id) {
         logger.debug("Вызван метов удаления студента");
         repository.deleteById(id);
     }
 
-    public Student editStudent(Student student){
+    public Student editStudent(Student student) {
         logger.debug("Вызван метов изменения студента");
         return repository.save(student);
     }
@@ -42,8 +43,8 @@ public class StudentService {
     public Collection<Student> findStudentsByAge(int age) {
         logger.debug("Вызван метов поиска студента по возрасту");
         List<Student> studentList = new ArrayList<>();
-        for (Student student: repository.findAll()) {
-            if (student.getAge()==age){
+        for (Student student : repository.findAll()) {
+            if (student.getAge() == age) {
                 studentList.add(student);
             }
 
@@ -52,21 +53,22 @@ public class StudentService {
 
     }
 
-    public Collection<Student> getStudentByName(String name){
+    public Collection<Student> getStudentByName(String name) {
         logger.debug("Вызван метов поиска студента по имени");
         return repository.getStudentsByName(name);
     }
 
-    public Collection<Student> findStudentsByAgeBetween(int minAge, int maxAge){
+    public Collection<Student> findStudentsByAgeBetween(int minAge, int maxAge) {
         logger.debug("Вызван метов поиска студентов с возрастными ограничениями");
         return repository.findByAgeBetween(minAge, maxAge);
     }
 
-    public int getCountOfStudent(){
+    public int getCountOfStudent() {
         logger.debug("Вызван метов подсчета количества студентов");
         return repository.countStudents();
     }
-    public double getAverageAge(){
+
+    public double getAverageAge() {
         logger.debug("Вызван метов получения среднего возраста студентов");
         return repository.findAll().stream()
                 .mapToDouble(Student::getAge)
@@ -74,7 +76,7 @@ public class StudentService {
                 .orElse(0d);
     }
 
-    public Collection<Student> findLastFiveStudents(){
+    public Collection<Student> findLastFiveStudents() {
         logger.debug("Вызван метов получения последних пяти студентов в базе");
         return repository.findLastFiveStudents();
     }
@@ -86,46 +88,73 @@ public class StudentService {
                 collect(Collectors.toList());
     }
 
+
     public void studentsThread() {
-        System.out.println("Name of the 0 student: " + repository.findAll().get(0).getName());
-        System.out.println("Name of the 1st student: " + repository.findAll().get(1).getName());
+        List<Student> studentList = repository.findAll();
+        logger.info("Name of the 0 student: " + studentList.get(0).getName());
+        logger.info("Name of the 1st student: " + studentList.get(1).getName());
 
         Thread thread1 = new Thread(() -> {
-            System.out.println("Name of the 2nd student: " + repository.findAll().get(2).getName());
-            System.out.println("Name of the 3rd student: " + repository.findAll().get(3).getName());
+            logger.info("Name of the 2nd student: " + studentList.get(2).getName());
+            logger.info("Name of the 3rd student: " + studentList.get(3).getName());
         });
         thread1.start();
 
         Thread thread2 = new Thread(() -> {
-            System.out.println("Name of the 4th student: " + repository.findAll().get(4).getName());
-            System.out.println("Name of the 5th student: " + repository.findAll().get(5).getName());
+            logger.info("Name of the 4th student: " + studentList.get(4).getName());
+            logger.info("Name of the 5th student: " + studentList.get(5).getName());
         });
         thread2.start();
+    }
+
+    public List<Student> getAll(){
+        return repository.findAll();
     }
 
     public void synchronizedStudentsThread() {
-        logger.debug("Called method doSynchronizedStudentsThread");
-        System.out.println("Name of the 0 student: " + repository.findAll().get(0).getName());
-        System.out.println("Name of the 1st student: " + repository.findAll().get(1).getName());
+        List<Student> students = repository.findAll();
 
         Thread thread1 = new Thread(() -> {
-            synchronized (StudentService.class) {
-                System.out.println("Name of the 2nd student: " + repository.findAll().get(2).getName());
-            }
-            synchronized (StudentService.class) {
-                System.out.println("Name of the 3rd student: " + repository.findAll().get(3).getName());
+            synchronized (this) {
+                for (int i = 0; i < students.size() / 2; i++) {
+                    Student student = getNextStudent(students);
+                    printStudent(student);
+                }
             }
         });
-        thread1.start();
 
         Thread thread2 = new Thread(() -> {
-            synchronized (StudentService.class) {
-                System.out.println("Name of the 4th student: " + repository.findAll().get(4).getName());
-            }
-            synchronized (StudentService.class) {
-                System.out.println("Name of the 5th student: " + repository.findAll().get(5).getName());
+            synchronized (this) {
+                for (int i = 0; i < students.size() / 2; i++) {
+                    Student student = getNextStudent(students);
+                    printStudent(student);
+                }
             }
         });
+
+        thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private synchronized Student getNextStudent(List<Student> list) {
+        if (counter >= list.size()) {
+            return null;
+        }
+        Student nextStudent = list.get(counter);
+        counter = (counter + 1) % list.size();
+        return nextStudent;
+    }
+
+    private synchronized void printStudent(Student student) {
+        logger.info("Name of the " + student.getId() + " student: " +  student.getName());
     }
 }
+
+
